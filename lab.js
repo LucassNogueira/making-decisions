5cf4e5fd3813a903a0d9da57b2d0d475532e7a40
LucassNogueira <simplylucasjason@gmail.com>
2022-01-05 15:07:24 -0600

    Problem 4 done

--- lab.js
+++ lab.js
@@ -53,15 +53,30 @@ if (amysBirthYear === brittanisBirthYear){
 // ////////// PROBLEM 4 //////////
 
 let temperature = 55
-let rain = true
+let rain = false
 
 // /*
-//   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
-//   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
+//   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, 
+//based on the above temperature and rain.
+//   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees,
+// and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. 
+//Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
 // */
 
 // //CODE HERE
-
+if(temperature >= 80 && rain === true){
+  console.log('Wear a t-shirt and bring an umberella!')
+} else if (temperature> 60-80 && rain === true){
+  console.log('Wear a rain-jacket.');
+} else if (temperature <= 60 && rain=== true){
+  console.log('One should wear a jacket and carry an umberella.')
+}else if (temperature >= 80 && rain === !true){
+  console.log('Wear a tank top!');
+}else if (temperature> 60-80 && rain === !true){
+  console.log('Wear a light jacket');
+}else if (temperature <= 60 && rain=== !true){
+  console.log('Bring a medium/heavy jacket.')
+}
 
 // ////////// PROBLEM 5 //////////
 
