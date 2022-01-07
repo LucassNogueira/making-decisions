748e19d158e3ec8758e72d05ea2aa7f9965d3d43
LucassNogueira <simplylucasjason@gmail.com>
2022-01-06 18:46:01 -0600

    problem 11

--- lab.js
+++ lab.js
@@ -1,33 +1,35 @@
-// ////////// PROBLEM 1 //////////
-// /*
-//   Create a variable called 'lovesCode' and set it equal to true. 
-//   Check to see if 'lovesCode' is equal to true or false. 
-//   If it is true, console log "I love to code!"
-//   If it is not, console log "Coding has its challenges."
-// */
+// // ////////// PROBLEM 1 //////////
+// // /*
+// //   Create a variable called 'lovesCode' and set it equal to true. 
+// //   Check to see if 'lovesCode' is equal to true or false. 
+// //   If it is true, console log "I love to code!"
+// //   If it is not, console log "Coding has its challenges."
+// // */
 
-// //CODE HERE
-// let lovesCode = true
-// if (lovesCode= true){
+// // //CODE HERE
+
+// let lovesCode =false
+
+// if (lovesCode === true){
 //   console.log('I love to code!');
 // } else {
 //   console.log('Coding has its challanges.');
 // }
 
 
-// // // For problems 2-3 use the following lines of code:
-// var amysAge = 29
-// var brittanisAge = 34
-// var amysBirthYear = 1991
-// var brittanisBirthYear = 1986
+// // // // For problems 2-3 use the following lines of code:
+var amysAge = 34
+var brittanisAge = 34
+var amysBirthYear = 1991
+var brittanisBirthYear = 1986
 
-// // ////////// PROBLEM 2 //////////
+// // // ////////// PROBLEM 2 //////////
 
-// // /*
-// //   Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. Handle this situation.
-// // */
+// // // /*
+// // //   Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. Handle this situation.
+// // // */
 
-// // //CODE HERE
+// // // //CODE HERE
 //  if (amysAge > brittanisAge){
 //    console.log("Amy is older.")}
 //     else if (amysAge < brittanisAge){
@@ -37,36 +39,36 @@
 //      console.log('Amy and Brittani are the same age!');
 //    }
 
-// // ////////// PROBLEM 3 //////////
+// // // ////////// PROBLEM 3 ////////f//
 
-// // /*
-// //   Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
-// // */
+// // // /*
+// // //   Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
+// // // */
 
-// // //CODE HERE
-// if (amysBirthYear === brittanisBirthYear){
-//   console.log('Amy & Brittani were born in the same year.');
-// } else {
-//   console.log('Amy and Brittani are not born in the same year.');
-// }
+// // // //CODE HERE
+// // if (amysBirthYear === brittanisBirthYear){
+// //   console.log('Amy & Brittani were born in the same year.');
+// // } else {
+// //   console.log('Amy and Brittani are not born in the same year.');
+// // }
 
-// // ////////// PROBLEM 4 //////////
+// // // ////////// PROBLEM 4 //////////
 
-// let temperature = 77
-// let rain = true
+let temperature = 70
+let rain = true
 
-// // /*
-// //   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, 
-// //based on the above temperature and rain.
-// //   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees,
-// // and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. 
-// //Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
-// // */
+// // // /*
+// // //   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, 
+// // //based on the above temperature and rain.
+// // //   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees,
+// // // and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. 
+// // //Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
+// // // */
 
-// // //CODE HERE
+// // // //CODE HERE
 // if(temperature >= 80 && rain === true){
 //   console.log('Wear a t-shirt and bring an umberella!')
-// } else if (temperature> 60-80 && rain === true){
+// } else if (temperature> 60 && temperature< 80 && rain === true){
 //   console.log('Wear a rain-jacket.');
 // } else if (temperature <= 60 && rain=== true){
 //   console.log('One should wear a jacket and carry an umberella.')
@@ -78,52 +80,52 @@
 //   console.log('Bring a medium/heavy jacket.')
 // }
 
-// // ////////// PROBLEM 5 //////////
+// // // ////////// PROBLEM 5 //////////
 
-// // /*
-// //   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
-// // */
+// // // /*
+// // //   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
+// // // */
 
-// // //CODE HERE
-// for (let i=0; i<10; i++){
-//   console.log('Hello');
-// }
-
-
-// // ////////// PROBLEM 6 //////////
-
-// // /*
-// //   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, 
-// //    on the first iteration it would log 1, on the second iteration it would log 2.
-// // */
-
-// // //CODE HERE
-// for (let i=1; i<11; i++){
-//   console.log(i);
-// }
+// // // //CODE HERE
+// // for (let i=0; i<10; i++){
+// //   console.log('Hello');
+// // }
 
 
-// // ////////// PROBLEM 7 //////////
+// // // ////////// PROBLEM 6 //////////
 
-// // /*
-// //   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
-// // */
+// // // /*
+// // //   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, 
+// // //    on the first iteration it would log 1, on the second iteration it would log 2.
+// // // */
 
-// // //CODE HERE
+// // // //CODE HERE
+// // for (let i=1; i<11; i++){
+// //   console.log(i);
+// // }
 
-// for (let i=10; i>=1; i--){
-//   console.log(i);
-// }
+
+// // // ////////// PROBLEM 7 //////////
+
+// // // /*
+// // //   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
+// // // */
+
+// // // //CODE HERE
+
+// // for (let i=10; i>=1; i--){
+// //   console.log(i);
+// // }
 
 
-// // ////////// PROBLEM 8 //////////
+// // // ////////// PROBLEM 8 //////////
 
-// // /*
-// //   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. 
-// //  In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
-// // */
+// // // /*
+// // //   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. 
+// // //  In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+// // // */
 
-// // //CODE HERE
+// // // //CODE HERE
 // let score = 0
 // let passingScore = 7
 
@@ -133,18 +135,18 @@
 //   score+=1
 // }
 
-// ////////// INTERMEDIATE PROBLEMS //////////
+// // ////////// INTERMEDIATE PROBLEMS //////////
 
-// ////////// PROBLEM 9 //////////
-// /*
-//   Create a variable called 'changeMyMind' and set it equal to true. 
-//   Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
+// // ////////// PROBLEM 9 //////////
+// // /*
+// //   Create a variable called 'changeMyMind' and set it equal to true. 
+// //   Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
 
-// */
+// // */
 
-// //CODE HERE
+// // //CODE HERE
 
-let changeMyMind = false
+// let changeMyMind = false
 
 
 // if (changeMyMind === true){
@@ -156,30 +158,30 @@ let changeMyMind = false
 // }
 
 
-// ////////// PROBLEM 10 //////////
-// // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
+// // ////////// PROBLEM 10 //////////
+// // // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
-// //CODE HERE
+// // //CODE HERE
 
-if(changeMyMind === true){
-  changeMyMind = !true;
-  console.log(changeMyMind);
-} else if (changeMyMind !== true){
-  changeMyMind = true;
-  console.log(changeMyMind)
-}
-// testing
-// ////////// ADVANCED PROBLEMS //////////
-//howdy
-// ////////// PROBLEM 11 //////////
+// if(changeMyMind === true){
+//   changeMyMind = !true;
+//   console.log(changeMyMind);
+// } else if (changeMyMind !== true){
+//   changeMyMind = true;
+//   console.log(changeMyMind)
+// }
+// // ////////// ADVANCED PROBLEMS //////////
+// // ////////// PROBLEM 11 //////////
 
 let z = 5
-// // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// // // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-// //CODE HERE
-
+// // //CODE HERE
+//set up a for loop inside while set i in for loop to z
 
 while (z > 0){
-  console.log(z)
+  console.log(z);
   z--
+  for (let i = z; i > 0; i--)
+  console.log(i);
 }
